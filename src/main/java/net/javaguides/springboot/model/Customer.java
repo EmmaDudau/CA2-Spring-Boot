8e5de8a710939a428c1c320661084eed999b3b26
jiashun-nci <73401615+jiashun-nci@users.noreply.github.com>
2020-12-08 16:39:46 +0000

    Added customer table mapping in spring

---
+++ src/main/java/net/javaguides/springboot/model/Customer.java
@@ -0,0 +1,106 @@
+package net.javaguides.springboot.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "customers")
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int customer_id;
+
+    @Column(name = "firstname")
+    private String firstName;
+
+    @Column(name = "surname")
+    private String surName;
+
+    @Column(name = "address")
+    private String address;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "phone")
+    private String phone;
+
+    @Column(name = "createDate")
+    private String createDate;
+
+    public Customer() {
+
+    }
+
+    public Customer(int customer_id, String firstName, String surName, String address, String email, String phone, String createDate) {
+        super();
+        this.customer_id = customer_id;
+        this.firstName = firstName;
+        this.surName = surName;
+        this.address = address;
+        this.email = email;
+        this.phone = phone;
+        this.createDate = createDate;
+    }
+
+    public long getCustomer_id() {
+        return customer_id;
+    }
+
+    public void setCustomer_id(long customer_id) {
+        this.customer_id = customer_id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSurName() {
+        return surName;
+    }
+
+    public void setSurName(String surName) {
+        this.surName = surName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(String createDate) {
+        this.createDate = createDate;
+    }
+}
