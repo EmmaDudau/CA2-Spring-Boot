12dccf136058ac7382d681f50fb5867a7fe5d2a3
danielozac <danielozac@gmail.com>
2021-01-01 11:49:38 +0000

    add SQL data load on server start

---
+++ src/main/resources/data.sql
@@ -0,0 +1,10 @@
+INSERT INTO products (product_name, product_quantity, product_modified, product_created, product_price, product_barcode )
+VALUES ('PS5', 10, '01/02/2021', '01/02/2021', 350, 'xyz456' );
+
+INSERT INTO products (product_name, product_quantity, product_modified, product_created, product_price, product_barcode )
+VALUES ('Iphone X', 50, '01/02/2021', '01/02/2021', 980, 'zyx123' );
+
+
+
+
+
